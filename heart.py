c5d108a7316714d8a56d7d9cff6b2c3fdc16a555
er1ika <killka224@yandex.ru>
2025-03-20 09:18:53 +0300

    heart.py

--- heart.py
+++ heart.py
@@ -1,18 +1,16 @@
 import turtle
 import math
 import time
-
-def draw_heart(x_scale, y_scale, offset_range, y_offset=0):
+def draw_heart(x_scale, y_scale, x2, y2=0):
     for t in range(0, 360, 5):
         angle = math.radians(t)
         x = x_scale * math.sin(angle) ** 3
         y = y_scale * (math.cos(angle) - 0.5 * math.cos(2 * angle) - 0.2 * math.cos(3 * angle) - 0.1 * math.cos(4 * angle))
-        for offset in offset_range:
+        for offset in x2:
             turtle.penup()
-            turtle.goto(x * 10 + offset, (y * 10 + offset + y_offset))
+            turtle.goto(x * 10 + offset, (y * 10 + offset + y2))
             turtle.pendown()
-            turtle.goto(x * 10 + offset, (y * 10 - offset + y_offset))
-
+            turtle.goto(x * 10 + offset, (y * 10 - offset + y2))
 def draw_letter_b():
     turtle.penup()
     turtle.goto(0, 140)
@@ -34,7 +32,6 @@ def draw_letter_b():
     turtle.penup()
     turtle.goto(0, 160)
     turtle.pendown()
-
 def draw_heart_outline():
     turtle.speed(0)
     turtle.width(1)
@@ -45,7 +42,7 @@ def draw_heart_outline():
     turtle.penup()
     turtle.goto(0, 150)
     turtle.pendown()
-    draw_heart(10, 8, range(-2, 3), y_offset=150)
+    draw_heart(10, 8, range(-2, 3), y2=150)
     turtle.penup()
     turtle.goto(-10, 160)
     turtle.pendown()
@@ -53,5 +50,4 @@ def draw_heart_outline():
     draw_letter_b()
     turtle.hideturtle()
     turtle.done()
-
 draw_heart_outline()
